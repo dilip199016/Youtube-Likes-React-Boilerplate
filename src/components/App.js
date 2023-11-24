f183cc5dc546f7c945a174ebfca0acc0679b1d7e
Dilip-Love-Code <118705027+dilip199016@users.noreply.github.com>
2023-11-25 00:47:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 import like from '../like.svg';
 
 const App = () => {
-  const [count,setCount] = React.useState();
-  
+  const [count, setCount] = useState(0);
+  const [backgroundColor, setBackgroundColor] = useState('rgba(255, 0, 0, 0)');
+
+  const handleLikeClick = () => {
+    setCount(count + 1);
+    const opacity = Math.min(1, (count + 1) * 0.1); // Ensure opacity doesn't exceed 1
+    setBackgroundColor(`rgba(255, 0, 0, ${opacity})`);
+  };
+
   return (
     <div id="main">
-      <img id="like-btn-img" style={{fill:'white',width:'100px',backgroundColor:'rgba(255,0,0,0)'}} />
-      <h3>Likes: <span id="like-counter">0</span></h3>
+      <img
+        id="like-btn-img"
+        src={like}
+        alt="Like Button"
+        style={{ fill: 'white', width: '100px', backgroundColor }}
+        onClick={handleLikeClick}
+      />
+      <h3>
+        Likes: <span id="like-counter">{count}</span>
+      </h3>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
